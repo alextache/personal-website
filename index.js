ba9e126f691ac146e1f1ac874710c66b31b3efc1
Alexandru Tache <77678776+alextache@users.noreply.github.com>
2021-02-18 17:50:21 +0000

    update

--- index.js
+++ index.js
@@ -4,8 +4,21 @@
 
  const server = http.createServer((req, res) => {
     if(req.url === '/') {
-      res.write(200, {'Content-Type': 'text/html'});
-      res.end('<h1>Home</h1>');
+      // load html page
+      fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content) => {
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(content);
+      })
+    }
+
+    if(req.url === '/about') {
+      // load html page
+      fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,content) => {
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(content);
+      })
     }
  });
 
