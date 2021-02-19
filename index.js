24d4ce363085d39ee6d21aaa0c2c5db4925e59a4
Alexandru Tache <77678776+alextache@users.noreply.github.com>
2021-02-19 16:40:25 +0000

    update

--- index.js
+++ index.js
@@ -3,23 +3,54 @@
  const fs = require('fs'); 
 
  const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-      // load html page
-      fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content) => {
-        if (err) throw err;
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(content);
-      })
-    }
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
-    if(req.url === '/about') {
-      // load html page
-      fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,content) => {
-        if (err) throw err;
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(content);
-      })
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial content type
+  let contentType = 'text/html';
+
+  // Check ext and set content type 
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+  }
+
+  // Read file
+  fs.readFile(filePath, (err, content) => {
+    if(err){
+      if(err.code == 'ENOENT') {
+        // Page not found
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(content, 'utf8');
+        })
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(content, 'utf8');
     }
+  });
+
  });
 
  const PORT = process.env.PORT || 5000;
